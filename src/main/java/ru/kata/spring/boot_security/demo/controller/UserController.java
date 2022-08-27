d0989757c149c26a5483501708ce343f8e34a505
Acc3l3rat0r <kraje.robert@gmail.com>
2022-08-27 21:36:26 +0300

    1.3 bootstrap jquery

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,11 +1,10 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -39,15 +38,8 @@ public class UserController {
         return "redirect:/admin";
     }
 
-    @GetMapping("/admin/showFormForUpdate/{id}")
-    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
-        User user = userService.getUserById(id);
-        model.addAttribute("user", user);
-        return "update_user";
-    }
-
-    @GetMapping("/admin/deleteUser/{id}")
-    public String deleteUser(@PathVariable(value = "id") long id) {
+    @PostMapping("/admin/deleteUser")
+    public String deleteUser(@RequestParam(value = "id") long id) {
         this.userService.deleteUserById(id);
         return "redirect:/admin";
     }
